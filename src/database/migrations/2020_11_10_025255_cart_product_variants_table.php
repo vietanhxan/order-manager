c3f25b5071632ad573292401eacfe0ffa5db864c
vietanhxan <Pham Viet Anh>
2020-11-10 10:00:00 +0700

    add table cart_product_variants

---
+++ src/database/migrations/2020_11_10_025255_cart_product_variants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CartProductVariantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_product_variants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('cart_item_id');
+            $table->unsignedBigInteger('variants_id');
+            $table->unsignedBigInteger('variants_type');
+            $table->foreign('cart_item_id')->references('id')->on('cart_items')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_product_variants');
+    }
+}
