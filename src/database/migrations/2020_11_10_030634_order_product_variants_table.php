4401a9de3ee94376dcb08831fbe1632b7aecc2bd
vietanhxan <Pham Viet Anh>
2020-11-10 10:09:42 +0700

    add table order_product_variants

---
+++ src/database/migrations/2020_11_10_030634_order_product_variants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class OrderProductVariantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_product_variants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('order_item_id');
+            $table->unsignedBigInteger('variant_id');
+            $table->unsignedBigInteger('variant_type');
+            $table->foreign('order_item_id')->references('id')->on('order_items')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_product_variants');
+    }
+}
